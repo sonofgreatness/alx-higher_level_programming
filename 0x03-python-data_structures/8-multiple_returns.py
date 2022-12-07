17c50ca0bee61c5b9c6f569b3a92b1a18f67464d
root <root@kali.simphiwe>
2022-12-07 06:39:09 +0200

    task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    len_ = len(sentence)
+    if len_ == 0:
+        returnstring = None
+    returnstring = sentence[0]
+    return (len_, returnstring)
