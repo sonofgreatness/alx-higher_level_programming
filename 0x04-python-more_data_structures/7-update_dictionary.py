0c62d610e3679c29cf2d7499a6ee75548aa4e525
root <root@kali.simphiwe>
2022-12-08 08:31:58 +0200

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function that replaces or adds key/value in a dictionary.
+key argument will be always a string
+value argument will be any type
+
+
+"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
