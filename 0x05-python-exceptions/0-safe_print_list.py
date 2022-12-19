8209eea24c9d17f2b4406e9b7b54163e3060d670
root <root@kali.simphiwe>
2022-12-19 19:03:04 +0200

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+        print()
+        return (x)
+    except xIsBiggerThanList:
+        for element in my_list:
+            counter = counter + 1
+            print("{}".format(element), end="")
+        print()
+        return (counter)
