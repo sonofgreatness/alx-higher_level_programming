347aba03d19cc7d6a31406eed54bb56633839a96
root <root@kali.simphiwe>
2022-12-20 06:45:51 +0200

    task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as er:
+        print("Exception: ", end="", file=sys.stderr)
+        print(er, file=sys.stderr)
+        return None
