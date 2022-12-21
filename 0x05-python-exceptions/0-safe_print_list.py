bd2ae267435deb09358a9bad189c7a918a0b1058
root <root@kali.simphiwe>
2022-12-21 07:02:29 +0200

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,5 +10,5 @@ def safe_print_list(my_list=[], x=0):
         for element in my_list:
             counter = counter + 1
             print("{}".format(element), end="")
-        print()
+        print("")
         return (counter)
