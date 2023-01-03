9132c94df1227c29b16c369aa615da3dfdd0ea51
sonofgreatness <73963799+sonofgreatness@users.noreply.github.com>
2023-01-03 19:14:42 +0200

    task 30

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class LockedClass:
+ """
+ Locked Class definition
+ Attributes : None
+
+ """
+    __slots__ = ['first_name']
+    def __init__(self, value)
+            self.first_name = value
