fd08f79a39cf30165d9df7ca1e3a30e7515c7093
root <root@kali.simphiwe>
2023-01-10 12:13:35 +0200

    task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+save_to_json_file that  writes an Object to a
+text file, using a JSON representation:
+
+Args
+    my_obj  - object to write
+    filename (string) - name of file
+
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w', encoding="UTF-8"):
+        json.dump(my_obj, filename)
