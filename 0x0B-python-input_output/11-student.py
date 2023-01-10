09776a68e5583c3fd1811aa7752e9b7a1f990838
root <root@kali.simphiwe>
2023-01-10 17:57:20 +0200

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+
+class Student:
+    """
+    Defines A student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes Object
+            Args =  attribute
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Args
+            attrs -> list of attributes
+        Returns  ddictionary representation of object
+        """
+        if attrs is None:
+            return (self.__dict__)
+
+        my_dict = []
+        if isinstance(attrs, list):
+            for e in attrs:
+                if not isinstance(e, str):
+                    return (self.__dict__)
+            for k, v in self.__dict__.items():
+                t = (k, v)
+                for e in attrs:
+                    if e == k:
+                        my_dict.append(t)
+            return dict(my_dict)
+
+    def reload_from_json(self, json):
+        """
+        reads file (as json ) and sets object attributes to
+        json values
+
+        Args
+            json  dictionary to read from
+
+        """
+
+        my_list = []
+        for k, v in self.__dict__.items():
+            for k2, v2 in json.items():
+                t = (k2, v2)
+                if (k is k2):
+                    my_list.append(t)
+            self.__dict__ = dict(my_list)
